e08ffb56dc0a301ea44aac25c904e87db0fe8366
SrShadowy <43967231+SrShadowy@users.noreply.github.com>
2020-01-27 22:32:56 -0300

    Compilado com GCC

---
+++ args.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+#include <fstream>
+#include <windows.h>
+
+int main(int argc, char *argv[])
+{
+           SetConsoleTitle("Captura de parametros.");
+           
+     std::cout << "Obtendo argumentos(parametros)..."
+     <<"\nQuantidade de argumentos obtido: " << argc;
+     
+     if (argc >= 2)
+     {
+          for(int i = 0; i < argc; ++i)
+               std::cout << std::endl << argv[i];
+               
+     std::ofstream out("1a.bat");
+     out << "start ";
+     
+           for(int i = 0; i < argc; ++i)
+               out << argv[i] <<  " ";
+     
+     }
+     else
+     {
+     std::cout << "\n\tError: 0x1\nArgumentos insuficiente"
+     << "\n\n\tAperte qualquer tecla para fechar\n";
+     std::cin.get(); 
+      return 1;    
+     }
+     
+     std::cout << "\n\nEscrito com sucesso... Arquivo criado '1a.bat' execute" 
+     << " apos fechar e abra o arquivo de renomeia novamente"
+     <<"\n\n\tAperte qualquer tecla para fecha\n";
+     
+     std::cin.get(); 
+     
+ return 0;    
+}
